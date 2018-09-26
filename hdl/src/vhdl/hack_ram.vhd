1799e5638175a97fba78b884511861dc203df201
zluudg <leonfe@kth.se>
2018-09-26 23:37:49 +0200

    Added Behaviuoral architecture

--- hdl/src/vhdl/hack_ram.vhd
+++ hdl/src/vhdl/hack_ram.vhd
@@ -11,6 +11,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.hack_shared.all;
 
 entity hack_ram is
@@ -19,11 +20,9 @@ entity hack_ram is
          ena : in STD_LOGIC;
          enb : in STD_LOGIC;
          wea : in STD_LOGIC;
-         web : in STD_LOGIC;
          addra : in STD_LOGIC_VECTOR(addrWidthRAM-1 downto 0);
          addrb : in STD_LOGIC_VECTOR(addrWidthRAM-1 downto 0);
          dia : in word;
-         dib : in word;
          doa : out word;
          dob : out word);
 end hack_ram;
@@ -31,7 +30,25 @@ end hack_ram;
 architecture Behavioral of hack_ram is
     signal ram : RamType := initMemoryFromFile(ramFile, 2**addrWidthRAM-1);
 begin
+    a_side: process(clka)
+    begin
+        if rising_edge(clka) then
+	    if ena = '1' then
+                doa <= ram(conv_integer(addra));
+                if wea = '1' then
+                    ram(conv_integer(addra)) <= dia;
+                end if;
+            end if;
+        end if;
+    end process a_side;
 
-    -- then processes for each clock
+    b_side: process(clkb)
+    begin
+        if rising_edge(clkb) then
+	    if enb = '1' then
+                dob <= ram(conv_integer(addrb));
+            end if;
+        end if;
+    end process b_side;
 
 end Behavioral;
