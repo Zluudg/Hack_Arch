a6ed7d855893e4a2579765b2da1992db4ab2cdec
zluudg <leonfe@kth.se>
2018-09-30 00:23:50 +0200

    Started writing buffer to RAM

--- hdl/src/vhdl/hack_videoif.vhd
+++ hdl/src/vhdl/hack_videoif.vhd
@@ -26,6 +26,42 @@ entity hack_videoif is
 end hack_videoif;
 
 architecture Behavioural of hack_videoif is
-begin
+    constant rows : integer := 16;
+    constant cols : integer := 80;
+    constant screenStart : integer := 16384;
+    constant screenEnd : integer := 17663;
 
+    type state_type is (s_idle, s_ready, s_fetch);
+
+    signal address : integer := screenStart;
+    signal freeSlots : integer := 20;
+    signal buff : STD_LOGIC_VECTOR(19 downto 0);
+    signal state : state_type := s_idle;
+    signal next_state : state_type := s_idle;
+    signal bufOut : STD_LOGIC_VECTOR(4 downto 0);
+    signal readBuf : STD_LOGIC := '0';
+    signal bufRdy : STD_LOGIC := '0';
+begin
+    ramEN <= '1'; -- RAM always enabled
+    lcdRW <= '0'; -- Always in WRITE mode
+
+    BUF: process (clk)
+    begin
+        case state is
+            when s_idle =>
+                if freeSlots >= 16 then
+                    next_state <= s_fetch;
+                    bufRdy <= '0';
+                elsif readBuf <= '1' then
+                    next_state <= s_fetch;
+                    bufOut <= -- TODO add code for output
+                else
+                    next_state <= s_idle;
+            when s_ready =>
+                -- TODO code
+            when s_fetch => 
+                -- TODO code
+        end case;
+        state <= next_state;
+    end process BUF;
 end Behavioural;
