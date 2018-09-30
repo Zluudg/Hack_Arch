0776fdea00a0e4b3eb8d250d06aa618c15de8332
zluudg <leonfe@kth.se>
2018-09-30 13:42:52 +0200

    IO management finished but UNTESTED

--- hdl/src/vhdl/hack_iomgmt.vhd
+++ hdl/src/vhdl/hack_iomgmt.vhd
@@ -25,10 +25,77 @@ entity hack_iomgmt is
          rdyKey : out STD_LOGIC;
          dataBuf : out word;
          enRAM : out STD_LOGIC;
-         rwRAM : out STD_LOGIC;
-         addrRAM : out STD_LOGIC);
+         wenRAM : out STD_LOGIC;
+         addrRAM : out STD_LOGIC_VECTOR(addrWidthRAM-1 downto 0));
 end hack_iomgmt;
 
 architecture Behavioural of hack_iomgmt is
+    type state_type is (s_idle, s_handleKey, s_handleBuf);
+
+    signal state : state_type := s_idle;
+    signal next_state : state_type := s_idle;
 begin
+    P_DECODE_NEXT: process(clk)
+    begin
+        if rising_edge(clk) then
+            case state is
+                when s_idle =>
+                    if reqKey = '1' then
+                        next_state <= s_handleKey;
+                    elsif reqBuf = '1' then
+                        next_state <= s_handleBuf;
+                    else
+                        next_state <= s_idle;
+                    end if;
+                when s_handleKey =>
+                    if reqBuf = '1' then
+                        next_state <= s_handleBuf;
+                    else
+                        next_state <= s_idle;
+                    end if;
+                when s_handleBuf =>
+                    next_state <= s_idle;
+                when others =>
+                    next_stace <= s_idle;
+            end case;
+        end if;
+    end process P_DECODE_NEXT;
+    
+    P_UPDATE: process (clk)
+    begin
+        if rising_edge(clk) then
+            state <= next_state;
+        end if;
+    end process P_UPDATE;
+
+    P_DECODE_OUT: process(state)
+    begin
+        case state is
+            when s_idle =>
+                rdyKey <= '0';
+                rdyBuf <= '0';
+                enRAM <= '0';
+            when s_handleKey =>
+                rdyKey <= '1';
+                rdyBuf <= '0';
+                enRAM <= '1';
+                wenRAM <= '1';
+                dataRAM <= dataKey;
+                addrRAM <= "110000000000000"; --address in RAM where keyboard output resides
+            when s_handleBuf =>
+                rdyKey <= '0';
+                rdyBuf <= '1';
+                enRAM <= '1';
+                wenRAM <= '0';
+                dataBuf <= dataRAM;
+                addrRAM <= reqAddrBuf;
+            when others =>
+                rdyKey <= '0';
+                rdyBuf <= '0';
+                enRAM <= '0';
+                wenRAM <= '0';
+                dataRAM <= (others => '0');
+                addrRAM <= (others => '0');
+        end case;
+    end process P_DECODE_OUT;
 end Behavioural;
